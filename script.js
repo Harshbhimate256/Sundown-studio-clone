53c9f4331d7eae8d1ab78e839ee36062368f15a9
Harsh Bhimate <harshbhimate2018@gmail.com>
2024-01-06 00:30:08 +0530

    added swiper

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ elems.forEach(function(e){
         fixed.style.backgroundImage = `url(${image})`
     })
 })
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100
+      });
+}
+swiperAnimation();
